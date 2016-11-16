e34c7a3108fe869358e54f564f4f99e1f8b01c5d
emdavis7 <ericd@simpli.fi>
2016-11-16 17:30:02 -0600

    Fail decode test

--- ceaser_test.py
+++ ceaser_test.py
@@ -5,3 +5,7 @@ import ceaser
 def test_encode():
     """ Tests the encode function of the script """
     assert ceaser.encode("Eric", 3) == "Hulf"
+
+def test_decode():
+    """ Tests the decode function of the script """
+    assert ceaser.decode("Hulf", 3) == "Eric"
