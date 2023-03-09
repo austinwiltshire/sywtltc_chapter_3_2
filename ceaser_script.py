28753fc20cf6ce3e9184c007d03157d8ac2bfb87
wynne-sifi <wynne@simpli.fi>
2023-03-09 10:36:39 -0700

    Restructure function as a definition with arguments choice and word

--- ceaser_script.py
+++ ceaser_script.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python
 
+"""This script prompts a user to enter a message to encode or decode
+using a classic Caesar shift substitution (3 letter shift)"""
+
 import string
 
-shift = 3
-choice = raw_input("would you like to encode or decode?")
-word = raw_input("Please enter text")
-letters = string.ascii_letters + string.punctuation + string.digits
-encoded = ""
-if choice == "encode":
-    for letter in word:
-        if letter == " ":
-            encoded = encoded + " "
-        else:
-            x = letters.index(letter) + shift
-            encoded = encoded + letters[x]
-if choice == "decode":
-    for letter in word:
-        if letter == " ":
-            encoded = encoded + " "
-        else:
-            x = letters.index(letter) - shift
-            encoded = encoded + letters[x]
-print(encoded)
 
+def ceaser(choice, word):
+    letters = string.ascii_letters + string.punctuation + string.digits
+    encoded = ""
+    if choice == "encode":
+        for letter in word:
+            if letter == " ":
+                encoded = encoded + " "
+            else:
+                x = letters.index(letter) + shift
+                encoded = encoded + letters[x]
+    if choice == "decode":
+        for letter in word:
+            if letter == " ":
+                encoded = encoded + " "
+            else:
+                x = letters.index(letter) - shift
+                encoded = encoded + letters[x]
+    return encoded
