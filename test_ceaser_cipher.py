6465f9e90e609fb65ce4a895f08e1dd97965ccb1
wynne-sifi <wynne@simpli.fi>
2023-03-09 10:56:52 -0700

    Write passing test

--- test_ceaser_cipher.py
+++ test_ceaser_cipher.py
@@ -3,4 +3,10 @@ import ceaser_cipher
 
 
 def test_ceaser_cipher():
-    assert ceaser_cipher.ceaser('decode',ceaser_cipher.ceaser('encode','statement')) == 'not_statement'
+    assert (
+        ceaser_cipher.ceaser("decode", ceaser_cipher.ceaser("encode", "statement"))
+        == "statement"
+    )
+
+
+li = [1, 2, 3, 4]
